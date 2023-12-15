83631745bd45b385973d5d77736ef5c3196978b4
sameer <sameerkhanjan123@gmail.com>
2023-12-15 21:14:57 +0500

    v4

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from App.views import *
 
 urlpatterns = [
-    path('display/', display_linked_list, name='display_linked_list'),
+    path('', display_linked_list, name='display_linked_list'),
     path('add_head/', add_node_to_head, name='add_node_to_front'),
     path('add_tail/', add_node_to_tail, name='add_node_to_tail'),
     path("delete_head/", delete_head, name="delete_head"),
